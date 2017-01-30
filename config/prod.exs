f71e17ef7ee206a3440878a7cb73365fae3c5120
userimack <mahendra.k12@gmail.com>
2017-01-30 13:47:57 +0530

    changed to string from chars

--- config/prod.exs
+++ config/prod.exs
@@ -16,14 +16,14 @@ config :rumbl, Rumbl.Endpoint,
   url: [scheme: "https", host: "rumblapp.herokuapp.com", port: 443],
   force_ssl: [rewrite_on: [:x_forwarded_proto]],
   cache_static_manifest: "priv/static/manifest.json",
-  secret_key_base: System.get_env('SECRET_KEY_BASE')
+  secret_key_base: System.get_env("SECRET_KEY_BASE")
 
 
 
 # Configure your database
 config :rumbl, Rumbl.Endpoint,
   adapters: Ecto.Adapters.Postgres,
-  url: System.get_env('DATABASE_URL'),
+  url: System.get_env("DATABASE_URL"),
   pool_size: String.to_integer(System.get_env('POOL_SIZE') || "10"),
   ssl: true
 # Do not print debug messages in production
