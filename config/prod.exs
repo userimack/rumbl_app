53c5073321d5d57f85b7921acd4647bb77dd7621
userimack <mahendra.k12@gmail.com>
2017-01-31 11:06:51 +0530

    Used EXRM for deploying the app

--- config/prod.exs
+++ config/prod.exs
@@ -14,7 +14,10 @@ use Mix.Config
 config :rumbl, Rumbl.Endpoint,
   http: [port: {:system, "PORT"}],
   url: [host: "example.com", port: 80],
-  cache_static_manifest: "priv/static/manifest.json"
+  root: ".",
+  cache_static_manifest: "priv/static/manifest.json",
+  server: true,
+  version: Mix.Project.config[:version]
 
 # Do not print debug messages in production
 config :logger, level: :info
@@ -48,7 +51,7 @@ config :logger, level: :info
 # If you are doing OTP releases, you need to instruct Phoenix
 # to start the server for all endpoints:
 #
-#     config :phoenix, :serve_endpoints, true
+   config :phoenix, :serve_endpoints, true
 #
 # Alternatively, you can configure exactly which server to
 # start per endpoint:
