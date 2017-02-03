15def619fd08b86867578e3087c2d415647a8ed0
userimack <mahendra.k12@gmail.com>
2017-02-03 17:16:23 +0530

    Created video addition, associated the videos with the user.

--- web/controllers/user_controller.ex
+++ web/controllers/user_controller.ex
@@ -1,6 +1,6 @@
 defmodule Rumbl.UserController do
   use Rumbl.Web, :controller
-  plug :authenticate when action in [:index, :show]
+  plug :authenticate_user when action in [:index, :show]
   
   alias Rumbl.User
 
@@ -32,14 +32,14 @@ defmodule Rumbl.UserController do
     end
   end
 
-  defp authenticate(conn, _opts) do
-    if conn.assigns.current_user do
-      conn
-    else
-      conn
-      |> put_flash(:error, "You must be logged in to access that page.")
-      |> redirect(to: page_path(conn, :index))
-      |> halt()
-    end
-  end
+  #  defp authenticate(conn, _opts) do
+  #    if conn.assigns.current_user do
+  #      conn
+  #    else
+  #      conn
+  #      |> put_flash(:error, "You must be logged in to access that page.")
+  #      |> redirect(to: page_path(conn, :index))
+  #      |> halt()
+  #    end
+  #  end
 end
--- web/router.ex
+++ web/router.ex
@@ -21,6 +21,12 @@ defmodule Rumbl.Router do
     resources "/users", UserController, only: [:index, :show, :new, :create]
     resources "/sessions", SessionController, only: [:new,:create, :delete]
   end
+   
+  scope "/manage", Rumbl do
+    pipe_through [:browser, :authenticate_user]
+
+    resources "/videos", VideoController
+  end
 
   # Other scopes may use custom stacks.
   # scope "/api", Rumbl do
